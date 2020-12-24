78470b971051373ed82ae6bb4bfc356066689dfe
qunlong <jinqunlong@zuzuche.com>
2020-12-24 11:47:10 +0800

    抽象搜索迭代器

---
+++ src/main/java/club/kingon/sql/opensearch/AbstractSearcherClientQueryIterator.java
@@ -0,0 +1,13 @@
+package club.kingon.sql.opensearch;
+
+/**
+ * @author dragons
+ * @date 2020/12/23 17:52
+ */
+public abstract class AbstractSearcherClientQueryIterator implements SearcherClientQueryIterator {
+
+    @Override
+    public void remove() {
+
+    }
+}
