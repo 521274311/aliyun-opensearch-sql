278bd450837937971a2409b5367602347107be09
qunlong <jinqunlong@zuzuche.com>
2020-12-24 11:46:14 +0800

    OpenSearch 搜索迭代器定义

---
+++ src/main/java/club/kingon/sql/opensearch/SearcherClientQueryIterator.java
@@ -0,0 +1,13 @@
+package club.kingon.sql.opensearch;
+
+
+import java.util.Iterator;
+
+/**
+ * 搜索链
+ * @author dragons
+ * @date 2020-12-23 17:43
+ */
+public interface SearcherClientQueryIterator extends Iterator<String> {
+
+}
